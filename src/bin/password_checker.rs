40897b1b3bfd55917eec429758c0494f1d649945
dev-sys <142793020+dev-sys-code@users.noreply.github.com>
2026-03-27 09:30:08 +0000

    Refactor password_checker for improved readability

--- src/bin/password_checker.rs
+++ src/bin/password_checker.rs
@@ -1,5 +1,4 @@
 use std::io;
-
 fn password_strength(password: &str) {
     let mut pass_score = 0;
     if password.len() >= 8 {
@@ -21,15 +20,12 @@ fn password_strength(password: &str) {
         _ => println!("Error"),
     }
 }
-
 fn main() {
     let mut password = String::new();
     println!("Enter password");
     io::stdin()
         .read_line(&mut password)
         .expect("Error");
-
     let password = password.trim();
-
     password_strength(password);
 }
